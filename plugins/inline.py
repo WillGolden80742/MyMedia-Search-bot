8d5a2981979c40e38e76e5af10e1585fc6b5bdf0
WillGolden80742 <william80742@gmail.com>
2022-07-15 20:58:47 -0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -38,8 +38,6 @@ async def answer(bot, query):
     reply_markup = get_reply_markup(bot.username, query=text)
     files, next_offset = await get_search_results(text, file_type=file_type, max_results=10, offset=offset)
 
-
-
     for file in files:
         results.append(
             InlineQueryResultCachedDocument(
