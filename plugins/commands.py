3f8cce9b26525cac52e5d8f0ac1b48d269f6a907
WillGolden80742 <william80742@gmail.com>
2022-07-15 20:12:18 -0300

    testando

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,8 +22,8 @@ async def start(bot, message):
             buttons = [[
                 InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
                 InlineKeyboardButton('Go Inline', switch_inline_query=''),
-                InlineKeyboardButton('1',message.reply("eu to maluco")),
-                InlineKeyboardButton('3',message.reply("eu to maluco")),
+                InlineKeyboardButton('Subscribe', callback_data='subscribe'),
+                InlineKeyboardButton('Unsubscribe', callback_data='unsubscribe'),
             ]]
             reply_markup = InlineKeyboardMarkup(buttons)
             await message.reply(START_MSG, reply_markup=reply_markup)
