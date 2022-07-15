80fe733683e045a5c0d03958f37cd1f008fc6f65
WillGolden80742 <william80742@gmail.com>
2022-07-14 23:25:10 -0300

    trazendo titulo de noticias

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,7 +40,7 @@ async def news(bot, message):
     try:
         request = requests.get("https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey="+NEWSAPI_ID)
         news = json.loads(request.content)
-        await message.reply(news)
+        await message.reply(news['title'])
     except Exception as e:
         await message.reply(e)        
 
