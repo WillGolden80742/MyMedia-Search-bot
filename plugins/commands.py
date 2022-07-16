38eb0029cc154d51ef2727a52e3880899eb917b1
WillGolden80742 <william80742@gmail.com>
2022-07-16 11:32:41 -0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -76,9 +76,9 @@ async def gnews(bot, message):
         for item in news['articles']:
             #get image of articles if exists and add title as caption and url if not exists send only title and url
             if item['urlToImage']:
-                await message.reply_photo(item['urlToImage'], caption="*"+item['title']+"*"+"\n"+item['description']+"\n"+item['url'])
+                await message.reply_photo(item['urlToImage'], caption="*bold*"+item['title']+"\n"+item['description']+"\n"+item['url'])
             else:
-                await message.reply("*"+item['title']+"*"+"\n"+item['description']+"\n"+item['url'])
+                await message.reply("*bold*"+item['title']+"\n"+item['description']+"\n"+item['url'])
     except Exception as e:
         await message.reply(e)        
 
