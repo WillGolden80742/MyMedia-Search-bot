ee01fa74be06ba3196bbbcd708f8d5cab1400625
WillGolden80742 <william80742@gmail.com>
2022-07-20 23:00:57 -0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -126,7 +126,7 @@ async def crypt(text,key,option="e"):
         if (keyPosition==keySize):
             keyPosition=0             
     if option == "e":
-        textCrypt = base64.b64encode(bytes(textCrypt,'utf-8'))
+        textCrypt =str(base64.b64encode(bytes(textCrypt,'utf-8')))
         textCrypt=textCrypt.replace("b'","")
         textCrypt=textCrypt[:-1]
         return textCrypt
